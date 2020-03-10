aab7dadaaf6412d581c88ffdacee7d2e6d7ad2b9
Karine Valenca <valenca.karine@gmail.com>
2020-03-09 21:11:43 -0300

    started project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Header } from './src/components/common'
+
+
+const App = () => {
+    return (
+      <Text>Hello</Text>
+
+    )
+}
+
+export default App 
