4f8cb0d9f6cd38d85ebadfdb18105bbc7929c0a6
Karine Valenca <valenca.karine@gmail.com>
2020-03-15 14:47:14 -0300

    finishing project

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,31 @@
 import React, { useContext, useEffect } from 'react'
-import { Text, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
+import { 
+    Text, 
+    StyleSheet, 
+    TouchableWithoutFeedback, 
+    View
+} from 'react-native'
 import { CardSection } from './common'
 import { Context } from '../context/LibraryContext'
+
+
 
 const ListItem = ({ library }) => {
     const { state, selectLibrary } = useContext(Context)
 
-    const { id, title } = library
+    const { id, title, description } = library
+
+
+
+    const renderDescription = () => {
+        if (id === state.selectedLibrary) {
+            return (
+                <CardSection>
+                    <Text style= {{ flex: 1}}>{description}</Text>
+                </CardSection>
+            )
+        }
+    }
 
     return (
         <TouchableWithoutFeedback onPress={() => selectLibrary(id)}>
@@ -14,7 +33,9 @@ const ListItem = ({ library }) => {
                 <CardSection>
                     <Text style={styles.titleStyle}>{title}</Text>
                 </CardSection>
+                {renderDescription()}
             </View>
+            
         </TouchableWithoutFeedback>
     )
 }
